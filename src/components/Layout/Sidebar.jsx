7ae4ffb5de21ffe3e48dc5514f162125483e4b27
AashaniSamarakoon <ashanisamarakoon36gmail.com>
2025-04-29 03:21:19 +0530

    updated

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -6,49 +6,54 @@ import {
     faUtensils,
     faUsers,
     faCog,
-    faSignOutAlt, faClipboardList, faPiggyBank, faInfoCircle
+    faSignOutAlt, 
+    faClipboardList, 
+    faPiggyBank, 
+    faInfoCircle
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import logo from './logo.png'; // Make sure to add your logo image in the same folder
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <div className="sidebar-logo">FoodDash</div>
+            <div className="sidebar-logo">
+                <img src={logo} alt="Quick Serve Logo" className="logo-image" />
+                <span>Quick Serve</span>
+            </div>
             <nav className="sidebar-nav">
                 <Link to="/dashboard" className="nav-item">
-                    <FontAwesomeIcon icon={faHome} />
+                    <FontAwesomeIcon icon={faHome} className="nav-icon" />
                     <span>Dashboard</span>
                 </Link>
                 <Link to="/dashboard/menu" className="nav-item">
-                    <FontAwesomeIcon icon={faUtensils} />
+                    <FontAwesomeIcon icon={faUtensils} className="nav-icon" />
                     <span>Menu Items</span>
                 </Link>
-
                 <Link to="/dashboard/orders" className="nav-item">
-                    <FontAwesomeIcon icon={faClipboardList} />
+                    <FontAwesomeIcon icon={faClipboardList} className="nav-icon" />
                     <span>Orders</span>
                 </Link>
                 <Link to="/dashboard/banking" className="nav-item">
-                    <FontAwesomeIcon icon={faPiggyBank} />
+                    <FontAwesomeIcon icon={faPiggyBank} className="nav-icon" />
                     <span>Banking</span>
                 </Link>
-
                 <Link to="/dashboard/customers" className="nav-item">
-                    <FontAwesomeIcon icon={faUsers} />
+                    <FontAwesomeIcon icon={faUsers} className="nav-icon" />
                     <span>Customers</span>
                 </Link>
                 <Link to="/dashboard/about" className="nav-item">
-                    <FontAwesomeIcon icon={faInfoCircle} />
+                    <FontAwesomeIcon icon={faInfoCircle} className="nav-icon" />
                     <span>About Us</span>
                 </Link>
                 <Link to="/dashboard/settings" className="nav-item">
-                    <FontAwesomeIcon icon={faCog} />
+                    <FontAwesomeIcon icon={faCog} className="nav-icon" />
                     <span>Settings</span>
                 </Link>
             </nav>
             <div className="sidebar-footer">
                 <a href="#" className="nav-item">
-                    <FontAwesomeIcon icon={faSignOutAlt} />
+                    <FontAwesomeIcon icon={faSignOutAlt} className="nav-icon" />
                     <span>Logout</span>
                 </a>
             </div>
@@ -57,65 +62,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-
-
-//
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './styles/Sidebar.css';
-// import {
-//     faHome,
-//     faUtensils,
-//     faUsers,
-//     faCog,
-//     faSignOutAlt, faClipboardList, faPiggyBank, faInfoCircle
-// } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//
-// const Sidebar = () => {
-//     return (
-//         <div className="sidebar">
-//             <div className="sidebar-logo">FoodDash</div>
-//             <nav className="sidebar-nav">
-//                 <Link to="/" className="nav-item">
-//                     <FontAwesomeIcon icon={faHome} />
-//                     <span>Dashboard</span>
-//                 </Link>
-//                 <Link to="/menu" className="nav-item">
-//                     <FontAwesomeIcon icon={faUtensils} />
-//                     <span>Menu Items</span>
-//                 </Link>
-//
-//                 <Link to="/orders" className="nav-item">
-//                     <FontAwesomeIcon icon={faClipboardList} />
-//                     <span>Orders</span>
-//                 </Link>
-//                 <Link to="/banking" className="nav-item">
-//                     <FontAwesomeIcon icon={faPiggyBank} />
-//                     <span>Banking</span>
-//                 </Link>
-//
-//                 <Link to="/customers" className="nav-item">
-//                     <FontAwesomeIcon icon={faUsers} />
-//                     <span>Customers</span>
-//                 </Link>
-//                 <Link to="/about" className="nav-item">
-//                     <FontAwesomeIcon icon={faInfoCircle} />
-//                     <span>About Us</span>
-//                 </Link>
-//                 <Link to="/settings" className="nav-item">
-//                     <FontAwesomeIcon icon={faCog} />
-//                     <span>Settings</span>
-//                 </Link>
-//             </nav>
-//             <div className="sidebar-footer">
-//                 <a href="#" className="nav-item">
-//                     <FontAwesomeIcon icon={faSignOutAlt} />
-//                     <span>Logout</span>
-//                 </a>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default Sidebar;
